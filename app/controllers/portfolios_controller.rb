606d053c527e475b1478a2d6c3cc76579ba8dca4
chunkster29 <chunkster29@googlemail.com>
2017-04-14 12:21:07 +0000

    Integrated delete action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,4 +19,33 @@ class PortfoliosController < ApplicationController
     end
   end
   
+  def edit
+     @portfolio_item = Portfolio.find(params[:id])
+  end
+  
+  def update
+     @portfolio_item = Portfolio.find(params[:id])
+    
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+  
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  
+  def destroy
+     @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Blog was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+  
 end
