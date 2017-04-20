438c935b6ac109715d2c7a3114d380c69755d536
chunkster29 <chunkster29@googlemail.com>
2017-04-20 15:14:25 +0000

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+    )
+  end
+  
+  puts "3 Topics created"
+
+
 10.times do |blog|
   Blog.create!(
     title: "My blog post #{blog}",
@@ -18,8 +27,8 @@
       ea commodi consequatur? Quis autem vel eum iure 
       reprehenderit qui in ea voluptate velit esse quam nihil 
       molestiae consequatur, vel illum qui dolorem eum fugiat 
-      quo voluptas nulla pariatur?"
-    
+      quo voluptas nulla pariatur?",
+      topic_id: Topic.last.id
     )
   end
   
@@ -34,10 +43,28 @@
   
   puts "5 skills post created"
   
-  9.times do |portfolio_item|
+  8.times do |portfolio_item|
     Portfolio.create!(
       title: "Portfolio title #{portfolio_item}",
-      subtitle: "My great service",
+      subtitle: "Ruby on rails",
+      body: "Lorem ipsum dolor sit amet, consectetur 
+        adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim 
+        veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui 
+        officia deserunt mollit anim id est laborum.",
+      main_image: "https://placehold.it/600x400",
+      thumb_image: "https://placehold.it/350x200"
+      )
+    end
+    
+    1.times do |portfolio_item|
+    Portfolio.create!(
+      title: "Portfolio title #{portfolio_item}",
+      subtitle: "Angular",
       body: "Lorem ipsum dolor sit amet, consectetur 
         adipiscing elit, sed do eiusmod tempor incididunt
         ut labore et dolore magna aliqua. Ut enim ad minim 
